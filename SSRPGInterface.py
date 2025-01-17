9fdc7491afad49cda3f887231936b6311312e08d
ArtificialPotato <zip.alt@gmail.com>
2025-01-17 12:23:58 +0900

    add mult_call
    add mult_call
    add version

--- SSRPGInterface.py
+++ SSRPGInterface.py
@@ -1,22 +1,64 @@
 import socket
-import select
-
+import itertools
 class SSRPGInterface:
     def __init__(self):
         self.host="127.0.0.1"
         self.port=12312
         self.step=None
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def sloppy_cast(self,_str):
+        if _str=="":
+            return _str
+        elif _str=="True":
+            return True
+        elif _str=="False":
+            return False
+        elif _str.isdecimal():
+            return int(_str)
+        elif _str[0]=="-" and _str[1:].isdecimal():
+            return -1*int(_str[1:])
+        return _str
     def callcommand(self,*args):
-        self.client.send('\x1f'.join(args).encode('utf-8'))
+        self.client.send(("1"+'\x1f'+'\x1f'.join(args)).encode('utf-8'))
         data = self.client.recv(1024)
-        if data.decode("utf-8")=="cmd_executed":
+        if data.decode("utf-8")[2:]=="cmd_done":
             return
     def call(self,*args):
-        self.client.send('\x1f'.join(args).encode('utf-8'))
+        sendstring="1"+'\x1f'+str(len(args)-1)+'\x1f'+args[0]
+        for i in range(1,len(args)):
+            if type(args[i])==int:
+                sendstring+='\x1f'+"i"+'\x1f'+str(args[i])
+            elif type(args[i]==str):
+                sendstring+='\x1f'+"s"+'\x1f'+args[i]
+            else:
+                print("callerr")
+                return
+        self.client.send(sendstring.encode('utf-8'))
         data = self.client.recv(1024)
-        print(data.decode('utf-8'))
-        return data.decode('utf-8')[4:]
+        #print(data.decode('utf-8'))
+        return self.sloppy_cast(data.decode('utf-8')[2:])
+    def multcall(self,args):
+        num=len(args)
+        sendstring=str(num)
+        for i in range(num):
+            sendstring+='\x1f'+str(len(args[i])-1)+'\x1f'+args[i][0]
+            for j in range(1,len(args[i])):
+                if type(args[i][j])==int:
+                    sendstring+='\x1f'+"i"+'\x1f'+str(args[i][j])
+                elif type(args[i][j]==str):
+                    sendstring+='\x1f'+"s"+'\x1f'+args[i][j]
+        #print(sendstring)
+        self.client.send(sendstring.encode('utf-8'))
+        data = self.client.recv(8192)
+        #print(data.decode('utf-8'))
+        return data.decode('utf-8').split('\x1f')[1:]
+    def getScreen(self,x,y,w,h):
+        inst=[["draw.GetSymbol",x+xy[1],y+xy[0]] for xy in itertools.product(range(h),range(w))]
+        res=self.multcall(inst)
+        res=["".join(res[w*i:w*(i+1)]) for i in range(h)]
+        res="\n".join(res)
+        return res
+
     def eof(self):
         self.client.send("EOF".encode('utf-8'))
     def run(self):
@@ -27,7 +69,10 @@ class SSRPGInterface:
             try:
                 while True:
                     data = self.client.recv(1024)
-                    if "start_step"==data.decode('utf-8'):
+                    if "step:v0.1"==data.decode('utf-8'):
+                        self.step()
+                    elif "step" in data.decode('utf-8'):
+                        print("warn:wrong version")
                         self.step()
                     else:
                         print("error")
--- SSRPGtest.py
+++ SSRPGtest.py
@@ -1,16 +1,56 @@
 from SSRPGInterface import SSRPGInterface
 import time
+import itertools
+import os
 ssrpgIF=SSRPGInterface()
+ssrpgIF.y=0
+w=30
+h=15
+
 def teststep():
+    ssrpgIF.y=(ssrpgIF.y+1)%30
     print("foe:"+ssrpgIF.call("foe"))
-    print("foe.distance:"+ssrpgIF.call("foe.distance"))
+    print("foe.distance:"+str(ssrpgIF.call("foe.distance")))
+    if ssrpgIF.call("loc.begin"):
+        ssrpgIF.callcommand("brew","tar + wood")
     if "boss" in ssrpgIF.call("foe"):
         ssrpgIF.callcommand("equipR","sword")
         ssrpgIF.callcommand("equipL","hammer")
     else:
         ssrpgIF.callcommand("equip","arm")
-    ssrpgIF.callcommand(">","hello python!"+time.ctime())
+        if ssrpgIF.call("foe.distance")<8 and ssrpgIF.call("item.CanActivate","skeleton_arm"):
+            ssrpgIF.callcommand("activate","R")
+        print(str(ssrpgIF.call("item.GetCooldown","skeleton_arm"))+" "+str(ssrpgIF.call("item.CanActivate","skeleton_arm")))
+    ssrpgIF.callcommand(">","`1,"+str(ssrpgIF.y)+","+"hello python!"+time.ctime())
     ssrpgIF.eof()
 
-ssrpgIF.step=teststep
+def teststep2():
+    os.system("cls")
+    ret=ssrpgIF.multcall(ssrpgIF.inst)
+    print("SSRPGpy:")
+    for i in range(h):
+        print("".join(ret[w*i:w*(i+1)]))
+    ssrpgIF.eof()
+
+def teststep3():
+    os.system("cls")
+    ssrpgIF.y=(ssrpgIF.y+1)%30
+    print("foe:"+ssrpgIF.call("foe"))
+    print("foe.distance:"+str(ssrpgIF.call("foe.distance")))
+    if ssrpgIF.call("loc.begin"):
+        ssrpgIF.callcommand("brew","tar + wood")
+    if "boss" in ssrpgIF.call("foe"):
+        ssrpgIF.callcommand("equipR","sword")
+        ssrpgIF.callcommand("equipL","hammer")
+    else:
+        ssrpgIF.callcommand("equip","arm")
+        if ssrpgIF.call("foe.distance")<8 and ssrpgIF.call("item.CanActivate","skeleton_arm"):
+            ssrpgIF.callcommand("activate","R")
+        print(str(ssrpgIF.call("item.GetCooldown","skeleton_arm"))+" "+str(ssrpgIF.call("item.CanActivate","skeleton_arm")))
+    ssrpgIF.callcommand(">","`1,"+str(ssrpgIF.y)+","+"hello python!"+time.ctime())
+    print(ssrpgIF.getScreen(15,7,30,15))
+    ssrpgIF.eof()
+test_inst=[["draw.GetSymbol",15+xy[1],7+xy[0]] for xy in itertools.product(range(h),range(w))]
+ssrpgIF.inst=test_inst
+ssrpgIF.step=teststep3
 ssrpgIF.run()
