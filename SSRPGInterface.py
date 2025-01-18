3751e695cb56e9dc2883f1a1237b1325f0433958
ArtificialPotato <zip.alt@gmail.com>
2025-01-18 18:01:02 +0900

    add comments

--- SSRPGInterface.py
+++ SSRPGInterface.py
@@ -1,76 +1,101 @@
 import socket
 import itertools
+
 class SSRPGInterface:
     def __init__(self):
-        self.host="127.0.0.1"
-        self.port=64649
-        self.step=None
-        self.ack_ver='\x06'+"0.1"
+        self.host = "127.0.0.1"
+        self.port = 64649
+        self.step = None
+        self.ack_ver = '\x06' + "0.1"
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    def sloppy_cast(self,_str):
-        if _str=="":
+
+    def sloppy_cast(self, _str):
+        """
+        Convert string to appropriate type.
+        """
+        if _str == "":
             return _str
-        elif _str=="True":
+        elif _str == "True":
             return True
-        elif _str=="False":
+        elif _str == "False":
             return False
         elif _str.isdecimal():
             return int(_str)
-        elif _str[0]=="-" and _str[1:].isdecimal():
-            return -1*int(_str[1:])
+        elif _str[0] == "-" and _str[1:].isdecimal():
+            return -1 * int(_str[1:])
         return _str
-    def callcommand(self,*args):
-        self.client.send(("1"+'\x1f'+'\x1f'.join(args)).encode('utf-8'))
+
+    def callcommand(self, *args):
+        """
+        Send a command to SSRPG.
+        """
+        self.client.send(("1" + '\x1f' + '\x1f'.join(args)).encode('utf-8'))
         data = self.client.recv(1024)
-        if data.decode("utf-8")[2:]=="cmd_done":
+        if data.decode("utf-8")[2:] == "cmd_done":
             return
-    def call(self,*args):
-        sendstring="1"+'\x1f'+str(len(args)-1)+'\x1f'+args[0]
-        for i in range(1,len(args)):
-            if type(args[i])==int:
-                sendstring+='\x1f'+"i"+'\x1f'+str(args[i])
-            elif type(args[i]==str):
-                sendstring+='\x1f'+"s"+'\x1f'+args[i]
+
+    def call(self, *args):
+        """
+        Call a function or get a variable from SSRPG.
+        """
+        sendstring = "1" + '\x1f' + str(len(args) - 1) + '\x1f' + args[0]
+        for i in range(1, len(args)):
+            if type(args[i]) == int:
+                sendstring += '\x1f' + "i" + '\x1f' + str(args[i])
+            elif type(args[i] == str):
+                sendstring += '\x1f' + "s" + '\x1f' + args[i]
             else:
                 print("callerr")
                 return
         self.client.send(sendstring.encode('utf-8'))
         data = self.client.recv(1024)
-        #print(data.decode('utf-8'))
         return self.sloppy_cast(data.decode('utf-8')[2:])
-    def multcall(self,args):
-        num=len(args)
-        sendstring=str(num)
+
+    def multcall(self, args):
+        """
+        Call multiple functions or get multiple variables from the server.
+        """
+        num = len(args)
+        sendstring = str(num)
         for i in range(num):
-            sendstring+='\x1f'+str(len(args[i])-1)+'\x1f'+args[i][0]
-            for j in range(1,len(args[i])):
-                if type(args[i][j])==int:
-                    sendstring+='\x1f'+"i"+'\x1f'+str(args[i][j])
-                elif type(args[i][j]==str):
-                    sendstring+='\x1f'+"s"+'\x1f'+args[i][j]
-        #print(sendstring)
+            sendstring += '\x1f' + str(len(args[i]) - 1) + '\x1f' + args[i][0]
+            for j in range(1, len(args[i])):
+                if type(args[i][j]) == int:
+                    sendstring += '\x1f' + "i" + '\x1f' + str(args[i][j])
+                elif type(args[i][j] == str):
+                    sendstring += '\x1f' + "s" + '\x1f' + args[i][j]
         self.client.send(sendstring.encode('utf-8'))
         data = self.client.recv(8192)
-        #print(data.decode('utf-8'))
         return data.decode('utf-8').split('\x1f')[1:]
-    def getScreen(self,x,y,w,h):
-        inst=[["draw.GetSymbol",x+xy[1],y+xy[0]] for xy in itertools.product(range(h),range(w))]
-        res=self.multcall(inst)
-        res=["".join(res[w*i:w*(i+1)]) for i in range(h)]
-        res="\n".join(res)
+
+    def getScreen(self, x, y, w, h):
+        """
+        Get the screen content from the server.
+        """
+        inst = [["draw.GetSymbol", x + xy[1], y + xy[0]] for xy in itertools.product(range(h), range(w))]
+        res = self.multcall(inst)
+        res = ["".join(res[w * i:w * (i + 1)]) for i in range(h)]
+        res = "\n".join(res)
         return res
 
     def eof(self):
+        """
+        End of frame.
+        """
         self.client.send('\x03'.encode('utf-8'))
+
     def run(self):
-        if self.step==None:
+        """
+        Start the interface and execute the step function in a loop.
+        """
+        if self.step is None:
             print("step() is none")
         else:
             self.client.connect((self.host, self.port))
             try:
                 while True:
                     data = self.client.recv(1024)
-                    if self.ack_ver==data.decode('utf-8'):
+                    if self.ack_ver == data.decode('utf-8'):
                         self.step()
                     elif '\x06' in data.decode('utf-8'):
                         print("warn:wrong version")
--- SSRPGtest.py
+++ SSRPGtest.py
@@ -1,31 +1,42 @@
 from SSRPGInterface import SSRPGInterface
 import time
 
-ssrpgIF=SSRPGInterface()
+# Initialize the SSRPGInterface
+ssrpgIF = SSRPGInterface()
 
-
+# Define the test step function
 def teststep():
+    # Check ?loc.begin
     if ssrpgIF.call("loc.begin"):
-        ssrpgIF.y=0
-        ssrpgIF.callcommand("brew","tar + wood")
+        ssrpgIF.y = 0
+        # Execute the command to brew tar + wood
+        ssrpgIF.callcommand("brew", "tar + wood")
     
-    ssrpgIF.y=(ssrpgIF.y+1)%30
-    ssrpgIF.callcommand(">","`1,"+str(ssrpgIF.y)+","+"hello python!"+time.ctime())
-
-    print("foe:"+ssrpgIF.call("foe"))
-    print("foe.distance:"+str(ssrpgIF.call("foe.distance")))
-
+    # Update y coordinate
+    ssrpgIF.y = (ssrpgIF.y + 1) % 30
+    
+    # Send a command with the y coordinate and current time
+    ssrpgIF.callcommand(">", "`1," + str(ssrpgIF.y) + "," + "hello python!" + time.ctime())
+    
+    # Print the foe and its distance
+    print("foe:" + ssrpgIF.call("foe"))
+    print("foe.distance:" + str(ssrpgIF.call("foe.distance")))
+    
+    # Equip different items based on the foe type
     if "boss" in ssrpgIF.call("foe"):
-        ssrpgIF.callcommand("equipR","sword")
-        ssrpgIF.callcommand("equipL","hammer")
+        ssrpgIF.callcommand("equipR", "sword")
+        ssrpgIF.callcommand("equipL", "hammer")
     else:
-        ssrpgIF.callcommand("equip","arm")
-        if ssrpgIF.call("foe.distance")<8 and ssrpgIF.call("item.CanActivate","skeleton_arm"):
-            ssrpgIF.callcommand("activate","R")
-        print(str(ssrpgIF.call("item.GetCooldown","skeleton_arm"))+" "+str(ssrpgIF.call("item.CanActivate","skeleton_arm")))
+        ssrpgIF.callcommand("equip", "arm")
+        if ssrpgIF.call("foe.distance") < 8 and ssrpgIF.call("item.CanActivate", "skeleton_arm"):
+            ssrpgIF.callcommand("activate", "R")
+        print(str(ssrpgIF.call("item.GetCooldown", "skeleton_arm")) + " " + str(ssrpgIF.call("item.CanActivate", "skeleton_arm")))
     
+    # End of frame
     ssrpgIF.eof()
 
+# Assign the test step function to the interface
+ssrpgIF.step = teststep
 
-ssrpgIF.step=teststep
+# Run the interface
 ssrpgIF.run()
